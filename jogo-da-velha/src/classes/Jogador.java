2a7c3800f6a42112611ad1664ba8477d1b0e59bf
Joaope Demesa <156481942+joaopedemesa@users.noreply.github.com>
2024-01-23 19:07:08 -0300

    Design parcialmente pronto

---
+++ jogo-da-velha/src/classes/Jogador.java
@@ -0,0 +1,33 @@
+package classes;
+
+public class Jogador {
+    private String nome;
+    private char simbolo;
+    private int jogada;
+
+    public int getJogada() {
+        return jogada;
+    }
+
+    public void setJogada(int jogada) {
+        this.jogada = jogada;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public char getSimbolo() {
+        return simbolo;
+    }
+
+    public void setSimbolo(char simbolo) {
+        this.simbolo = simbolo;
+    }
+    
+    
+}
